ba66f3bcd1a299994566ce78afcc034e422d0f23
Chris Klementis <chris_klementis@transcanada.com>
2015-12-11 15:26:58 -0700

    Getting ready to try to work with data. Separate out the buttons example into its own div and own js file.
    Here we have the basics of what we are looking to make on the GUI.

--- src/script.jsx
+++ src/script.jsx
@@ -1,44 +1,26 @@
-// Create a React component
-var Button = React.createClass({
-    localHandleClick: function() {
-        this.props.localHandleClick(this.props.increment);
-    },
-    render: function(){
-        return(
-            <button onClick={this.localHandleClick}>+{this.props.increment}</button>
-        )
-    }
-})
-
-// New component to just show the value of the counter
-var Result = React.createClass({
-    render: function(){
-        return(
-            <div>{this.props.localCounter}</div>
-        )
-    }
-})
-
-// Single Main component to include all the other components
-var Main = React.createClass({
-    getInitialState : function() {
-        return {counter: 0};
-    },
-    handleClick : function(anIncrement){
-        this.setState({ counter: this.state.counter + anIncrement });
-    },
-    render: function(){
-        return(
+var Card =  React.createClass({
+    render:function(){
+        return (
             <div>
-                <Button localHandleClick={this.handleClick} increment={1}/>
-                <Button localHandleClick={this.handleClick} increment={5}/>
-                <Button localHandleClick={this.handleClick} increment={10}/>
-                <Button localHandleClick={this.handleClick} increment={50}/>
-                <Button localHandleClick={this.handleClick} increment={100}/>
-                <Result localCounter={this.state.counter}/>
+                <img src="https://avatars.githubusercontent.com/u/239742?v=3" width="200"/>
+                <h3>Pete Hunt</h3>
+                <hr/>
+            </div>
+        );
+    }
+})
+
+var Main = React.createClass({
+    render: function(){
+        return (
+            <div>
+                <Card />
             </div>
         )
     }
-})
+});
 
 React.render(<Main />, document.getElementById("root"))
+
+
+//API URL with JSON data: https://api.github.com/users/petehunt
