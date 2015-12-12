4ee968e879db2acf27efe9c32a42b75d780b8540
Chris Klementis <chris_klementis@transcanada.com>
2015-12-11 17:16:39 -0700

    Now lets take in names into the array from a form element. create a new Form component to represent the form.

--- src/script.jsx
+++ src/script.jsx
@@ -17,18 +17,43 @@ var Card =  React.createClass({
             </div>
         );
     }
-})
+});
+
+var Form = React.createClass({
+    handleSubmit:function(e){
+        e.preventDefault(); // otherwise the form will submit normally and refresh the page...
+        var loginInputByUser = React.findDOMNode(this.refs.githubLoginRef);
+
+        // Add the card here
+        this.props.addCard(loginInputByUser.value);
+        loginInputByUser.value = ''; // reset the input to blank
+    },
+    render:function(){
+        return (
+          <form onSubmit={this.handleSubmit}>
+              <input placeholder="github login" ref="githubLoginRef"/>
+              <button>Add</button>
+          </form>
+        );
+    }
+});
 
 var Main = React.createClass({
     getInitialState:function(){
-        return { logins:['zpao','fisherwebdev'] };
+        return { logins:[] };
+    },
+    addCard:function(loginToAdd){
+        // grab the current states login array and push the new login into it and then set the state to the new array we just pushed an item to
+        this.setState({logins: this.state.logins.concat(loginToAdd)});
     },
     render: function(){
+        // For the Form component to be able to invoke the addCard function, we need to pass it in as a property
         var cards = this.state.logins.map(function(login){
             return (<Card login={login}/>)
         });
         return (
             <div>
+                <Form addCard={this.addCard}/>
                 {cards}
             </div>
         )
@@ -39,3 +64,5 @@ React.render(<Main />, document.getElementById("root"))
 
 
 //API URL with JSON data: https://api.github.com/users/petehunt
+
+// sample usernames: petehuny, zpao, spicyj
